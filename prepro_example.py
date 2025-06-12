df2e159cdc5ebcf0842c2b0ddc3fd9efd69e066c
Pablo Arias <>
2025-06-12 17:02:52 +0100

    prepro cross platform update

--- prepro_example.py
+++ prepro_example.py
@@ -5,7 +5,7 @@ from ducksoup import ds_process_parallel
 
 print("Starting analysis")
 
-session_name = "first_data_set" # COMPLETE
+session_name = "first_data_set/" # COMPLETE
 
 ds_process_parallel(sources = "data/"+session_name+"/*/recordings/", target_folder="preproc/"+session_name+"/", folder_tag= "session_name")
 
